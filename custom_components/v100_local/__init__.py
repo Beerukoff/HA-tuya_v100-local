5931de322f3ac422b20c908e4d6780a9a3fd22ef
Beerukoff <beerukoff@gmail.com>
2026-07-29 01:22:21 +0300

    Update __init__.py

--- custom_components/v100_local/__init__.py
+++ custom_components/v100_local/__init__.py
@@ -6,22 +6,20 @@ from homeassistant.core import HomeAssistant
 DOMAIN = "v100_local"
 
 
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up the integration."""
-    return True
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up V100 Local from a config entry."""
 
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    """Set up V100 from a config entry."""
     return True
 
 
 async def async_unload_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
+    hass: HomeAssistant, entry: ConfigEntry
 ) -> bool:
-    """Unload V100."""
+    """Unload a config entry."""
+
+    hass.data[DOMAIN].pop(entry.entry_id, None)
+
     return True
