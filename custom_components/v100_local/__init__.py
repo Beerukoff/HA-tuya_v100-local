c923aad216c56b7298de0abfdd5381fc434bf944
Beerukoff <beerukoff@gmail.com>
2026-07-29 01:30:54 +0300

    Update __init__.py

--- custom_components/v100_local/__init__.py
+++ custom_components/v100_local/__init__.py
@@ -5,9 +5,6 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .device import V100Device
-from .lock import V100Lock
-
 DOMAIN = "v100_local"
 
 
@@ -17,24 +14,12 @@ async def async_setup_entry(
 ) -> bool:
     """Set up V100."""
 
-    device = V100Device(
-        ip=entry.data["ip"],
-        device_id=entry.data["device_id"],
-        local_key=entry.data["local_key"],
-        version=entry.data.get("version", "3.4"),
-    )
-
-    lock = V100Lock(
-        device,
-        entry.title,
-    )
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
 
     await hass.config_entries.async_forward_entry_setups(
         entry,
         ["lock"],
     )
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = lock
 
     return True
