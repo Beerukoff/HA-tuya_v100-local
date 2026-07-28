46a8e799065f45c14a10d13868fac566415f20ae
Beerukoff <beerukoff@gmail.com>
2026-07-29 01:18:43 +0300

    Create const.py

---
+++ custom_components/v100_local/const.py
@@ -0,0 +1,13 @@
+"""Constants for V100 Local."""
+
+DOMAIN = "v100_local"
+
+CONF_IP = "ip"
+CONF_DEVICE_ID = "device_id"
+CONF_LOCAL_KEY = "local_key"
+CONF_PROTOCOL_VERSION = "protocol_version"
+
+DEFAULT_PROTOCOL_VERSION = "3.4"
+
+MANUFACTURER = "Tuya"
+MODEL = "V100"
