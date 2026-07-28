609dab7b8e99ee46fc61a80bc1fe2f32304d2eca
Beerukoff <beerukoff@gmail.com>
2026-07-29 01:43:37 +0300

    Update config_flow.py

--- custom_components/v100_local/config_flow.py
+++ custom_components/v100_local/config_flow.py
@@ -5,9 +5,15 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_IP_ADDRESS
 
-from .const import DOMAIN
+from .const import (
+    DOMAIN,
+    CONF_IP,
+    CONF_DEVICE_ID,
+    CONF_LOCAL_KEY,
+    CONF_PROTOCOL_VERSION,
+    DEFAULT_PROTOCOL_VERSION,
+)
 
 
 class V100LocalConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -15,21 +21,27 @@ class V100LocalConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+    async def async_step_user(
+        self,
+        user_input: dict | None = None,
+    ):
+        """Handle user setup."""
 
         if user_input is not None:
             return self.async_create_entry(
-                title="V100 Lock",
+                title=f"V100 {user_input[CONF_IP]}",
                 data=user_input,
             )
 
         schema = vol.Schema(
             {
-                vol.Required(CONF_IP_ADDRESS): str,
-                vol.Required("device_id"): str,
-                vol.Required("local_key"): str,
-                vol.Optional("version", default="3.4"): str,
+                vol.Required(CONF_IP): str,
+                vol.Required(CONF_DEVICE_ID): str,
+                vol.Required(CONF_LOCAL_KEY): str,
+                vol.Optional(
+                    CONF_PROTOCOL_VERSION,
+                    default=DEFAULT_PROTOCOL_VERSION,
+                ): str,
             }
         )
 
